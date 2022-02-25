5f8f3c5d2e21bf864c14c22de6a9412d5cec2ebb
Marco Jaros <marco.jaros@bict.ch>
2022-02-25 08:22:36 +0100

    yellowLetters updated

--- html/index.php
+++ html/index.php
@@ -4,6 +4,7 @@ $resetArray = array("","","","","");
 $yellowLetters = (isset($_GET['yellowLetters']))? $_GET['yellowLetters']:"";
 $grayLetters = (isset($_GET['grayLetters']))? $_GET['grayLetters']:0;
 $greenLettersArray = array(0 => (isset($_GET['greenLetter1']))? $_GET['greenLetter1']:"", 1 => (isset($_GET['greenLetter2']))? $_GET['greenLetter2']:"", 2 => (isset($_GET['greenLetter3']))? $_GET['greenLetter3']:"", 3 => (isset($_GET['greenLetter4']))? $_GET['greenLetter4']:"", 4 =>(isset($_GET['greenLetter5']))? $_GET['greenLetter5']:"");
+$yellowLettersArray = array(0 => (isset($_GET['yellowLetter1']))? $_GET['yellowLetter1']:"", 1 => (isset($_GET['yellowLetter2']))? $_GET['yellowLetter2']:"", 2 => (isset($_GET['yellowLetter3']))? $_GET['yellowLetter3']:"", 3 => (isset($_GET['yellowLetter4']))? $_GET['yellowLetter4']:"", 4 =>(isset($_GET['yellowLetter5']))? $_GET['yellowLetter5']:"");
 
 if(isset($_POST['reset'])) {
     $yellowLetters = "";
@@ -15,7 +16,11 @@ if(isset($_POST['reset'])) {
 ?>
 <container>
     <form action="index.php" method="GET">
-        <input type="text" name="yellowLetters" value = "<?if(empty($yellowLetters)){echo"";} else {echo $yellowLetters;} ?>">
+        <input type="text" name="yellowLetter1" class="yellowLetters" value = "<?if(empty($yellowLettersArray[0])){echo"";} else {echo $yellowLettersArray[0];} ?>">
+        <input type="text" name="yellowLetter2" class="yellowLetters" value = "<?if(empty($yellowLettersArray[1])){echo"";} else {echo $yellowLettersArray[1];} ?>">
+        <input type="text" name="yellowLetter3" class="yellowLetters" value = "<?if(empty($yellowLettersArray[2])){echo"";} else {echo $yellowLettersArray[2];} ?>">
+        <input type="text" name="yellowLetter4" class="yellowLetters" value = "<?if(empty($yellowLettersArray[3])){echo"";} else {echo $yellowLettersArray[3];} ?>">
+        <input type="text" name="yellowLetter5" class="yellowLetters" value = "<?if(empty($yellowLettersArray[4])){echo"";} else {echo $yellowLettersArray[4];} ?>">
         <input type="text" name="grayLetters" value = "<?if(empty($grayLetters)){echo"";} else {echo $grayLetters;} ?>">
         <input type="text" name="greenLetter1" class="greenLetters" maxlength="1" value = "<?if(empty($greenLettersArray[0])){echo"";} else {echo $greenLettersArray[0];} ?>">
         <input type="text" name="greenLetter2" class="greenLetters" maxlength="1" value = "<?if(empty($greenLettersArray[1])){echo"";} else {echo $greenLettersArray[1];} ?>">
@@ -43,7 +48,7 @@ if(isset($_POST['reset'])) {
     form {
         margin: 3px;
     }
-    input[name="yellowLetters"]{
+    input[class="yellowLetters"]{
         background-color: #c9b458;
     }
     input[name="grayLetters"]{
@@ -67,6 +72,7 @@ if(isset($_POST['reset'])) {
     container {
         display: flex;
     }
+
 </style>
 
 <?php
@@ -94,15 +100,36 @@ for($i = 0; $i < 5; $i++) {
 }
 $greenLettersQuery = $greenLettersQuery . '"';
 
+$yellowLettersArray0 = str_split($yellowLettersArray[0], $length = 1);
+$yellowLettersArray1 = str_split($yellowLettersArray[1], $length = 1);
+$yellowLettersArray2 = str_split($yellowLettersArray[2], $length = 1);
+$yellowLettersArray3 = str_split($yellowLettersArray[3], $length = 1);
+$yellowLettersArray4 = str_split($yellowLettersArray[4], $length = 1);
+
 
 //save letters from yellow/gray textbox in array
-$yellowLettersArray = str_split($yellowLetters, $length = 1);
+$yellowLettersArray2D = [
+    $yellowLettersArray0,
+    $yellowLettersArray1,
+    $yellowLettersArray2,
+    $yellowLettersArray3,
+    $yellowLettersArray4,
+];
+
+
+
 $grayLettersArray = str_split($grayLetters, $length = 1);
 
 //build part of the query with yellow letters
-for($i = 0; $i < count($yellowLettersArray); $i++) {
-    $yellowLettersQueryArray[$i] = 'AND word LIKE "%' . $yellowLettersArray[$i] . '%" ';
-    $yellowLettersQuery = $yellowLettersQuery . $yellowLettersQueryArray[$i];
+for($i = 0; $i < count($yellowLettersArray2D); $i++) {
+    for($j = 0; $j < count($yellowLettersArray2D[$i]); $j++) {
+    $yellowLettersQuery = $yellowLettersQuery .  'AND word LIKE "%' . $yellowLettersArray2D[$i][$j] . '%" ';
+    if($i == 0){$yellowLettersQuery = $yellowLettersQuery . 'AND word NOT LIKE "' . $yellowLettersArray2D[$i][$j] . '____" ';}
+    if($i == 1){$yellowLettersQuery = $yellowLettersQuery . 'AND word NOT LIKE "_' . $yellowLettersArray2D[$i][$j] . '___" ';}
+    if($i == 2){$yellowLettersQuery = $yellowLettersQuery . 'AND word NOT LIKE "__' . $yellowLettersArray2D[$i][$j] . '__" ';}
+    if($i == 3){$yellowLettersQuery = $yellowLettersQuery . 'AND word NOT LIKE "___' . $yellowLettersArray2D[$i][$j] . '_" ';}
+    if($i == 4){$yellowLettersQuery = $yellowLettersQuery . 'AND word NOT LIKE "____' . $yellowLettersArray2D[$i][$j] . '" ';}
+    }
 }
 
 //build part of the query with gray letters
